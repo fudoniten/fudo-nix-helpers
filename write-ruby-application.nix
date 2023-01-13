cc243e1075c24edb820874be63a74ad845e66349
niten <niten@fudo.org>
2023-01-13 09:51:35 -0800

    Set PATH for ruby script.
    Which requires wrapping the ruby script in a Bash script.

--- write-ruby-application.nix
+++ write-ruby-application.nix
@@ -1,30 +1,39 @@
 { name, text, pkgs, runtimeInputs ? [ ], checkPhase ? null, ... }:
-pkgs.writeTextFile {
-  inherit name runtimeInputs;
-  meta.mainProgram = name;
-  executable = true;
-  destination = "/bin/${name}";
-  text = ''
-    #!${pkgs.ruby_3_1}/bin/ruby
 
-    ${text}
-  '';
-  checkPhase = let
-    excludedChecks = [
-      "Style/ColonMethodCall"
-      "Style/BlockDelimiters"
-      "Style/StringLiterals"
-      "Naming/FileName"
-      "Layout/TrailingEmptyLines"
-      "Lint/ScriptPermission"
-    ];
-  in if checkPhase == null then ''
-    runHook preCheck
-    HOME=$(mktemp -d rubocop-XXXXXXXX)
-    ${pkgs.rubocop}/bin/rubocop --except ${
-      pkgs.lib.concatStringsSep "," excludedChecks
-    } --cache false "$target"
-    runHook postCheck
-  '' else
-    checkPhase;
-}
+with pkgs.lib;
+let
+  rubyExec = let ruby-name = "${name}-ruby";
+  in pkgs.writeTextFile {
+    name = ruby-name;
+    meta.mainProgram = ruby-name;
+    executable = true;
+    destination = "/bin/${ruby-name}";
+    text = ''
+      #!${pkgs.ruby_3_1}/bin/ruby
+
+      ${text}
+    '';
+    checkPhase = let
+      excludedChecks = [
+        "Style/ColonMethodCall"
+        "Style/BlockDelimiters"
+        "Style/StringLiterals"
+        "Naming/FileName"
+        "Layout/TrailingEmptyLines"
+        "Lint/ScriptPermission"
+      ];
+    in if checkPhase == null then ''
+      runHook preCheck
+      HOME=$(mktemp -d rubocop-XXXXXXXX)
+      ${pkgs.rubocop}/bin/rubocop --except ${
+        pkgs.lib.concatStringsSep "," excludedChecks
+      } --cache false "$target"
+      runHook postCheck
+    '' else
+      checkPhase;
+  };
+in pkgs.writeShellScriptBin name (optionalString (runtimeInputs != [ ]) ''
+  export PATH="${makeBinPath runtimeInputs}:$PATH"
+'' + ''
+  ${rubyExec}
+'')
