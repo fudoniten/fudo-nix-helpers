56347b3cddc9b4ee8bdc048932cb01bec810f866
niten <niten@fudo.org>
2023-03-02 10:38:14 -0800

    Forget the overlay

--- flake.nix
+++ flake.nix
@@ -16,29 +16,24 @@
           inherit system;
           overlays = [ self.overlays.default ];
         };
+
+        clj-pkgs = clj-nix.packages."${system}";
+
+        default-jdk = pkgs.jdk18_headless;
       in {
-        packages = { inherit (pkgs) mkClojureLib mkClojureBin; };
-        devShells.clojure = let
-          update-deps = pkgs.writeShellScriptBin "update-deps.sh"
+        packages = {
+          mkClojureLib = pkgs.callPackage ./clojure-lib.nix {
+            inherit (clj-pkgs) mkCljLib;
+            jdkRunner = default-jdk;
+          };
+          mkClojureBin = pkgs.callPackage ./clojure-bin.nix {
+            inherit (clj-pkgs) mkCljBin;
+            jdkRunner = default-jdk;
+          };
+          updateClojureDeps = pkgs.writeShellScriptBin "update-deps.sh"
             "${clj-nix.packages."${system}".deps-lock}/bin/deps-lock";
-        in pkgs.devshell.mkShell {
-          packages = with pkgs; [ clojure update-deps ];
         };
       }) // {
         lib = { writeRubyApplication = import ./write-ruby-application.nix; };
-        overlays.default = final: prev:
-          let
-            clj-pkgs = clj-nix.packages."${prev.system}";
-            default-jdk = prev.jdk18_headless;
-          in {
-            mkClojureLib = final.callPackage ./clojure-lib.nix {
-              inherit (clj-pkgs) mkCljLib;
-              jdkRunner = default-jdk;
-            };
-            mkClojureBin = final.callPackage ./clojure-bin.nix {
-              inherit (clj-pkgs) mkCljBin;
-              jdkRunner = default-jdk;
-            };
-          };
       };
 }
